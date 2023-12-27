aa8dcc470394d1bdbca9eca4e383e185e246ffdb
AntonioJadrejci <ajadrejcic@unipu.hr>
2023-12-27 17:56:35 +0100

    Home slika

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      Welcome to FamilyWings, where every place in Europe is not to far. We are
+      a commercial flight company from Croatia. Our fleet consists mostly of
+      Airbus A321 Neo and Airbus A350 types of planes. Our destinations branch
+      south to Spain all the way to Iceland on the north. Thank you for flying
+      with us :)
+    </h1>
   </div>
 </template>
