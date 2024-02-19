0779df8c159f258560ffd70b525436b51e08a363
AntonioJadrejci <ajadrejcic@unipu.hr>
2024-02-20 00:15:33 +0100

    Dodani glavni leta, renta i busa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="home-background"></div>
+  <div class="home-background">
+    <div class="squares-container">
+      <div class="square" @mouseover="scaleUp" @mouseout="scaleDown">
+        <img src="@/assets/plane.webp" alt="Image 1" class="square-image" />
+        <button class="square-button">Flight</button>
+      </div>
+      <div class="square" @mouseover="scaleUp" @mouseout="scaleDown">
+        <img src="@/assets/RentaCar.jpg" alt="Image 1" class="square-image" />
+        <button class="square-button">Rent a Car</button>
+      </div>
+      <div class="square" @mouseover="scaleUp" @mouseout="scaleDown">
+        <img src="@/assets/ShuttleBus.jpg" alt="Image 1" class="square-image" />
+        <button class="square-button">Shuttle Bus</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
   components: {},
+  methods: {
+    scaleUp(event) {
+      event.currentTarget.classList.add("hovered");
+    },
+    scaleDown(event) {
+      event.currentTarget.classList.remove("hovered");
+    },
+  },
 };
 </script>
 <style scoped>
@@ -17,5 +40,48 @@ export default {
   height: 100vh;
   width: 100%;
   border-radius: 0 !important;
+}
+.squares-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.square {
+  width: 400px;
+  height: 400px;
+  background-color: #ffffff;
+  margin: 60px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+}
+.square-image {
+  width: 400px;
+  height: 300px;
+  margin-bottom: 100px;
+  border-radius: 10px;
+}
+
+.square-button {
+  width: 350px;
+  height: 50px;
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #9400d3;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 25px;
+}
+.hovered {
+  transform: scale(1.1);
 }
 </style>
