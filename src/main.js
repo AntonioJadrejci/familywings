1104569c3df80671bfa9fc21c72a9cb1a4a4ea70
AntonioJadrejci <ajadrejcic@unipu.hr>
2024-03-26 14:50:51 +0100

    Dodavanje bootstrapa

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 library.add(faBars);
 
