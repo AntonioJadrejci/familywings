6636b575d6d5684b3f06272659b3a4b37e53b1ad
AntonioJadrejci <ajadrejcic@unipu.hr>
2024-04-01 18:26:04 +0200

    Greska u verzijama commit

--- src/main.js
+++ src/main.js
@@ -4,21 +4,19 @@ import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-library.add(faBars);
-
+// Create a new Vue app instance
 const app = createApp(App);
 
-app.use(router);
+// Use BootstrapVue and IconsPlugin with app.use()
+app.use(BootstrapVue);
+app.use(IconsPlugin);
 
-// Register the FontAwesome component globally
+// Register global components
+library.add(faBars);
 app.component('font-awesome-icon', FontAwesomeIcon);
 
+// Mount the app and specify the element to mount on
+app.use(router);
 app.mount('#app');
