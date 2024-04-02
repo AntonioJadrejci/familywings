76e7ad58eb74061c8760e6a5214dc7a4aeece90a
AntonioJadrejci <ajadrejcic@unipu.hr>
2024-04-02 14:48:55 +0200

    Fixan bootstrap

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
+// Step 1: Import BootstrapVue and IconsPlugin
+
+
+// Import Bootstrap and BootstrapVue CSS
+
+
+// Import FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+// Add FontAwesome icon to the library
 library.add(faBars);
 
+// Register global component for FontAwesome
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 // Use BootstrapVue and IconsPlugin with app.use()
-app.use(BootstrapVue);
-app.use(IconsPlugin);
 
-// Register global components
-library.add(faBars);
-app.component('font-awesome-icon', FontAwesomeIcon);
 
 // Mount the app and specify the element to mount on
 app.use(router);
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const webpack = require('webpack');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      // Define feature flags
+      new webpack.DefinePlugin({
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false // Set it to true if needed
+      })
+    ]
+  }
+};
