defe7aea2f8ad4342098b3b8443a8374cf14863d
AntonioJadrejci <ajadrejcic@unipu.hr>
2025-01-02 17:17:53 +0100

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src') // Definiše alias za "src" direktorij
+      }
+    },
     plugins: [
-      // Define feature flags
+      // Postojeći DefinePlugin za feature flags
       new webpack.DefinePlugin({
-        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false // Set it to true if needed
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false // Zadržava postojeću konfiguraciju
       })
     ]
   }
